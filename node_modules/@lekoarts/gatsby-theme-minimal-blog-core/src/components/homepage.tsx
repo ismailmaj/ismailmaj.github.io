2f5fba639148e88a584d11bc40c51246f65c9b5e
ismailmaj <ismailmaj98@gmail.com>
2023-03-19 00:34:30 +0100

    Updates

--- node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/components/homepage.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-
-export default function Homepage({ data }: { data: any }) {
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
-}
--- node_modules/@lekoarts/gatsby-theme-minimal-blog-core/src/components/tags.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-
-export default function Tags({ data }: { data: any }) {
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
-}
